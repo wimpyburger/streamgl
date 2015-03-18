f9c911f5653c2cf76e30071d99bbd1e4d839c4d4
wimpyburger <xump@null.net>
2015-03-18 22:02:42 +0000

    Made it actually work
    Fixed everything
    Added far more dashboard controls

--- server.js
+++ server.js
@@ -7,15 +7,75 @@ var port = 3000;
 server.listen(port);
 
 app.get('/dashboard', function(req, res) {
-	res.sendfile(__dirname + '/public/dashboard.html');
-});
-
-io.sockets.on('connection', function(socket) {
-	socket.on('show msg', function(data) {
-		console.log(data);
-	});
+	res.sendFile(__dirname + '/public/dashboard.html');
 });
 
 app.get('/notification', function(req, res) {
-	res.sendfile(__dirname + '/public/notification.html');
+	res.sendFile(__dirname + '/public/notification.html');
+});
+
+app.get('/logo.png', function(req, res) {
+	res.sendFile(__dirname + '/public/img/logo.png');
+});
+
+app.get('/wimpy.jpg', function(req, res) {
+	res.sendFile(__dirname + '/public/img/wimpy.jpg');
+});
+
+app.get('/brenman.jpg', function(req, res) {
+	res.sendFile(__dirname + '/public/img/brenman.jpg');
+});
+
+io.sockets.on('connection', function(socket) {
+	socket.on('headershow', function(data) {
+		io.emit('headershow', data);
+	});
+	
+	socket.on('headerhide', function(data) {
+		io.emit('headerhide', data);
+	});
+	
+	socket.on('show msg', function(data) {
+		io.emit('news', data);
+	});
+	
+	socket.on('hide', function(data) {
+		io.emit('hide', data);
+	});
+	
+	socket.on('showimg', function(data) {
+		io.emit('showimg', data);
+	});
+	
+	socket.on('hideimg', function(data) {
+		io.emit('hideimg', data);
+	});
+	
+	socket.on('transition', function(data) {
+		io.emit('transition', data);
+	});
+	
+	socket.on('showmembers', function(data) {
+		io.emit('showmembers', data);
+	});
+	
+	socket.on('hidemembers', function(data) {
+		io.emit('hidemembers', data);
+	});
+	
+	socket.on('backgroundshow', function(data) {
+		io.emit('backgroundshow', data);
+	});
+	
+	socket.on('backgroundhide', function(data) {
+		io.emit('backgroundhide', data);
+	});
+	
+	socket.on('followershow', function(data) {
+		io.emit('followershow', data);
+	});
+	
+	socket.on('followerhide', function(data) {
+		io.emit('followerhide', data);
+	});
 });
